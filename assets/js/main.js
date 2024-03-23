98aaa10828ebcfd2e098116d5cc73fe7b4e7a97d
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-03-23 14:31:22 -0600

    feat(): Agregando botón up
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
 // Agrega la clase "loaded" al body cuando la página haya cargado completamente
 window.addEventListener("load", function () {
-    document.body.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+// Boton UP
+document.addEventListener("DOMContentLoaded", function () {
+  const btnUp = document.getElementById("boton-up");
+
+  window.addEventListener("scroll", function () {
+    const scrollY = window.scrollY || window.pageYOffset;
+
+    if (scrollY >= 200) {
+      btnUp.style.bottom = "15px"; // Ajusta la posición según tus necesidades
+    } else {
+      btnUp.style.bottom = "-100px"; // Oculta el botón fuera de la pantalla
+    }
+  });
 });
